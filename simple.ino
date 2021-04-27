9a710fc3128f6044745c9e9f0015b8d26bcbc80f
Joshua Rice <joshuarice@users.noreply.github.com>
2021-04-27 14:19:04 -0600

    Create simple.ino

---
+++ simple.ino
@@ -0,0 +1,16 @@
+#include <FastLED.h>
+#include <pixeltypes.h>
+
+#define NUM_STRIPS 4
+#define NUM_LEDS_PER_STRIP 1
+CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
+
+void setup() {
+  delay(1000);
+  // WS2812 Type, PIN 7
+  FastLED.addLeds<WS2812,7,GRB>(leds[0], NUM_LEDS_PER_STRIP);
+}
+
+void loop() {
+    leds[0][0] = CRGB::Blue;
+}
